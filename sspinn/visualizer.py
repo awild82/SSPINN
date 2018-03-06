9f6421bff7ab7c9b2f5ad2960ca4bcabe9116a4d
torinfs <torins@uw.edu>
2018-03-05 16:36:02 -0800

    fixed input error checks

--- sspinn/visualizer.py
+++ sspinn/visualizer.py
@@ -7,16 +7,23 @@ from networkx.drawing.nx_agraph import graphviz_layout
 class visualizer:
 
     def __init__(self,full,empirical):
+
+        # Test input types
+        if not isinstance(full, np.ndarray):
+            raise TypeError('Input "full" is not a numpy matrix')
+        if not isinstance(empirical, dict):
+            raise TypeError('Input "empirical" is not a dictionary')
+
         self.full_mat = full
         self.empirical = empirical
         self.shift = {'H': 0, 'C': 183, 'N': 327, 'O': 346, 'F': 385, 'Cl': 397,\
                       'Br': 407, 'I': 413, 'P': 419, 'B': 421, 'S': 424}
         #self.shift = {'H': 0, 'C': 5, 'N': 9, 'O': 11} #FOR TEST CASE BELOW
-        self.genLabels()
-        self.truncate()
+        self._genLabels()
+        self._truncate()
 
 
-    def truncate(self):
+    def _truncate(self):
         '''
         Takes NN output matrix and truncates it to a minimal size
         adjacency matrix, used for plotting the output molecular
@@ -29,8 +36,8 @@ class visualizer:
             atomcount += self.empirical[key]
 
         # initialize numpy matrices
-        tempmat = np.zeros((13,atomcount),dtype=int) #FOR TEST CASE BELOW
-        #tempmat = np.zeros((432,atomcount),dtype=int) 
+        #tempmat = np.zeros((13,atomcount),dtype=int) #FOR TEST CASE BELOW
+        tempmat = np.zeros((432,atomcount),dtype=int) 
         adjmat = np.zeros((atomcount,atomcount),dtype=int)
 
         # Get the non-zero columns from the NN output adjacency matrix
@@ -56,7 +63,7 @@ class visualizer:
         self.adjmat = adjmat
 
 
-    def genLabels(self):
+    def _genLabels(self):
         '''
         Uses the empirical formula input to generate list of atomic 
         labels for use by draw2Dstructure().
@@ -79,8 +86,6 @@ class visualizer:
         label_dict = {}
         for i in range(len(self.labels)):
             label_dict[i] = self.labels[i]
-
-        print(label_dict, self.empirical)
 
         # Atom color keys
         atomcolor = {'C':'grey', 'O':'r', 'N':'b', 'H':'w', 'S':'orange', 'F':'y', 'Cl':'g', \
@@ -144,31 +149,4 @@ orgo[5,11] = 2
 vis = visualizer(orgo,empirical)
 vis.draw2Dstructure()
 '''
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
