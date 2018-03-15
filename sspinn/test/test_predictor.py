b4f0eecd3c4ff75cb84059a6b3ab3052724731e0
Ian Murphy <imurphy@uw.edu>
2018-03-15 00:03:08 -0700

    Added tests for setting custom nets. Expanded tests for __init__.

--- sspinn/test/test_predictor.py
+++ sspinn/test/test_predictor.py
@@ -18,6 +18,32 @@ def test__init__():
     assert len(test_predictor._nn.layers) != 0, \
         "Keral model did not fill layers"
 
+    # Tests that a filled keras model is loaded for testing
+    test_predictor = Predictor(train=False, test=True)
+    if test_predictor._data is None:
+        pass
+    else:
+        raise Exception('_data is not empty when testing')
+    if test_predictor.load_nn() is None:
+        pass
+    else:
+        raise Exception('Does not initiate empty string for writing \
+            trained net')
+
+
+def test_set_nn():
+    """Testing the safe method of setting custom nets"""
+
+    # Tests that setting new net is checked by ._check_net
+    test_predictor = Predictor(train=False, test=True)
+    nn = 'nn_string.h5'
+    try:
+        test_predictor.set_nn(nn)
+    except TypeError:
+        pass
+    else:
+        print('TypeError not handled for loading custom net')
+
 
 def test_load_nn():
     """Tests that correct error is thrown if  incorrect nn is loaded"""
